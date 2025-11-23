0f9db48aca4ae4eb6db89a85aee3fe270702063d
Varun Puchnanda <varun.puchnanda@gmail.com>
2025-11-23 20:55:52 +0300

    Modified index.html and vite.config.js files

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:3000"
+      //"/api": "http://localhost:3000"
+      "/api": "https://odooproduct-backend.onrender.com:3000"
     }
   }
 })
